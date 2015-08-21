c7d56d1df1732749a970c84b6b21ef168328dad4
tweetweetarnold <arnold.lee.wt@gmail.com>
2015-08-21 17:34:48 +0800

    updated simple model classes

---
+++ src/model/User.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package model;
+
+import java.util.Date;
+
+
+/**
+ * @author Arnold
+ *
+ */
+public class User {
+	private String username, password, name;
+	private Date createDate;
+
+}
