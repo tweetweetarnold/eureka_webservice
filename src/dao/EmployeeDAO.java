0e99c641ecad42e59a75eca34b00a5f67cb8110f
tweetweetarnold <arnold.lee.wt@gmail.com>
2015-08-26 14:35:07 +0800

    updated EmployeeDAO

--- src/dao/EmployeeDAO.java
+++ src/dao/EmployeeDAO.java
@@ -8,10 +8,12 @@ public class EmployeeDAO {
 	public EmployeeDAO() {
 	}
 
-	public static Employee getUser(int id) {
+	public static Employee getEmployee(int id) {
 		return (Employee) MyConnection.get(Employee.class, id);
 	}
-	
-	
+
+	public static void saveEmployee(Employee e) {
+		MyConnection.save(e);
+	}
 
 }
