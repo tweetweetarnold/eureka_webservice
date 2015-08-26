0c5327ed80efd48527c5587bbb57ee97b535dae2
bhkoh <boonhui.koh.2013@sis.smu.edu.sg>
2015-08-26 19:35:46 +0800

    added CompanyDAO.java file

---
+++ src/dao/CompanyDAO.java
@@ -0,0 +1,27 @@
+package dao;
+
+import connection.MyConnection;
+import entity.Company;
+
+public class CompanyDAO {
+	
+	public CompanyDAO() {
+		
+	}
+	
+	public static Company getCompany(int companyId) {
+		return (Company) MyConnection.get(Company.class, companyId);
+	}
+	
+	public static void saveCompany(Company c) {
+		MyConnection.save(c);
+	}
+	
+	public static void updateCompany(Company c) {
+		MyConnection.update(c);
+	}
+	
+	public static void deleteCompany(Company c) {
+		MyConnection.delete(c);
+	}
+}
