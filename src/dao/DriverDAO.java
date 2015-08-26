79cedf4272ecb1eb206a0aa94a74a49465439462
bhkoh <boonhui.koh.2013@sis.smu.edu.sg>
2015-08-26 19:37:05 +0800

    added DriverDAO.java file

---
+++ src/dao/DriverDAO.java
@@ -0,0 +1,27 @@
+package dao;
+
+import connection.MyConnection;
+import entity.Driver;
+
+public class DriverDAO {
+	
+	public DriverDAO() {
+		
+	}
+	
+	public static Driver getDriver(int driverId) {
+		return (Driver) MyConnection.get(Driver.class, driverId);
+	}
+	
+	public static void saveDriver(Driver d) {
+		MyConnection.save(d);
+	}
+	
+	public static void updateDriver(Driver d) {
+		MyConnection.update(d);
+	}
+	
+	public static void deleteDriver(Driver d) {
+		MyConnection.delete(d);
+	}
+}
