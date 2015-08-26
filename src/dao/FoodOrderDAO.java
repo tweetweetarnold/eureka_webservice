15dd8d3a8b3c8faafb0379c4aa4d45d235813db6
bhkoh <boonhui.koh.2013@sis.smu.edu.sg>
2015-08-26 19:43:21 +0800

    added FoodOrderDAO.java file

---
+++ src/dao/FoodOrderDAO.java
@@ -0,0 +1,27 @@
+package dao;
+
+import connection.MyConnection;
+import entity.FoodOrder;
+
+public class FoodOrderDAO {
+	
+	public FoodOrderDAO() {
+		
+	}
+	
+	public static FoodOrder getFoodOrder(int foodOrderId) {
+		return (FoodOrder) MyConnection.get(FoodOrder.class, foodOrderId);
+	}
+	
+	public static void saveFoodOrder(FoodOrder f) {
+		MyConnection.save(f);
+	}
+	
+	public static void updateFoodOrder(FoodOrder f) {
+		MyConnection.update(f);
+	}
+	
+	public static void deleteFoodOrder(FoodOrder f) {
+		MyConnection.delete(f);
+	}
+}
