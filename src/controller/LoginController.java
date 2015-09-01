d56963c1307dddea822cfa0d03d6180bb090de33
bhkoh <boonhui.koh.2013@sis.smu.edu.sg>
2015-09-01 13:12:53 +0800

    Updated the codes in authenticateUser method.

--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -1,11 +1,35 @@
 package controller;
 
+import dao.*;
+import entity.*;
+
 public class LoginController {
 	
 	public boolean authenticateUser(String username, String password) {
 		String testUsername = "arnold";
 		String testPassword = "123";
-		return (testUsername.equals(username) && testPassword.equals(password));
+		
+		//BH modified the authenticating codes for the case of employee login
+		//invoking the new getEmployeUsername method
+		Employee e = EmployeeDAO.getEmployeeUsername(username); 
+		if (e != null) {
+			String employeeUsername = e.getUsername();
+			if (employeeUsername != null) {
+				String employeePassword = e.getPassword();
+				if (employeePassword != null) {
+					return (employeeUsername.equals(username) && employeePassword.equals(password));
+				} else {
+					return false;
+				}
+			} else {
+				return false;
+			}
+		} else {
+			return false;
+		}
+		
+		
+		//return (testUsername.equals(username) && testPassword.equals(password));
 	}
 
 }
