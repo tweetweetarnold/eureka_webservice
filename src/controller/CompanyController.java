8cda551762be7bdf0c8101888f9bee17b953bd28
bhkoh <boonhui.koh.2013@sis.smu.edu.sg>
2015-09-13 22:56:04 +0800

    Created CompanyController.java with retrieve, update, delete method to invoke methods from CompanyDAO respectively

---
+++ src/controller/CompanyController.java
@@ -0,0 +1,24 @@
+package controller;
+
+import dao.*;
+import entity.*;
+
+public class CompanyController {
+	
+	public void addNewCompany(Company c) {
+		CompanyDAO.saveCompany(c);
+	}
+	
+	public void updateCompany(Company c) {
+		CompanyDAO.updateCompany(c);
+	}
+	
+	public void removeCompany(Company c) {
+		CompanyDAO.deleteCompany(c);
+	}
+	
+	public Company retrieveCompany(int companyId) {
+		Company c = CompanyDAO.getCompany(companyId);
+		return c;
+	}
+}
