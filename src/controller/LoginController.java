a3d9978d3690a3f252c0b3abb9b9774204f985a3
bhkoh <boonhui.koh.2013@sis.smu.edu.sg>
2015-10-05 11:16:26 +0800

    removed unused codes and comments in the login controller

--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -15,18 +15,15 @@ public class LoginController {
 	 * This method returns an Employee object upon a successful authentication, otherwise, it will return null
 	 */
 	public Employee authenticateUser (int id, String inputPassword)  {
-//		String testUsername = "arnold";
-//		String testPassword = "123";
+
 		Employee e = null;
 		e = EmployeeDAO.getEmployee(id);
-		System.out.println("Employee: " + e.getUsername());
 		
 		int employeeid = e.getEmployeeId();
 		if (employeeid != 0) {
 			String employeePasswordinDB = e.getPassword();
-			//checking that the input password is correct as the password stored in DB
+			//checking that the input password is correct as the password stored in DataBase
 			if (inputPassword.equals(employeePasswordinDB)) {
-				
 				return e;
 			} else {
 				return null;
