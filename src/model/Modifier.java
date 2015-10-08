14eb74ce1f2aa0d9ef04284f9a56cc90f52b0dbf
bunnykillher <bunnykillher@gmail.com>
2015-10-08 10:33:00 +0800

    Add Modifier model

---
+++ src/model/Modifier.java
@@ -0,0 +1,87 @@
+package model;
+
+import java.util.Date;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+@Entity
+public class Modifier {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int modifierId;
+	private String name;
+	private String description;
+	private double price;
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "foodId")
+	private Food food;
+	// private image byte[];
+	private Date createDate;
+
+	public Modifier() {
+	}
+
+	public Modifier(String name, String description, double price, Food food,
+			Date createDate) {
+		super();
+		this.name = name;
+		this.description = description;
+		this.price = price;
+		this.food = food;
+		this.createDate = createDate;
+	}
+
+	public int getModifierId() {
+		return modifierId;
+	}
+
+	public void setModifierId(int modifierId) {
+		this.modifierId = modifierId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public Food getFood() {
+		return food;
+	}
+
+	public void setFood(Food food) {
+		this.food = food;
+	}
+
+	public Date getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+
+}
