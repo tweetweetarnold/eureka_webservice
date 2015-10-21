f4f4fee1690aefaa83bae5af32c3814cda4356d2
bhkoh <boonhui.koh.2013@sis.smu.edu.sg>
2015-10-21 19:15:06 +0800

    added the email generator codes. ***Take note: need to manually import the javax.mail and javax activation framework .jar files from the eclipse plugin

---
+++ src/services/EmailGenerator.java
@@ -0,0 +1,82 @@
+package services;
+
+
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class EmailGenerator {
+
+	Properties emailProperties;
+	Session mailSession;
+	MimeMessage emailMessage;
+
+//	public static void main(String args[]) throws AddressException,
+//			MessagingException {
+//
+//		EmailGenerator javaEmail = new EmailGenerator();
+//
+//		javaEmail.setMailServerProperties();
+//		javaEmail.createEmailMessage();
+//		javaEmail.sendEmail();
+//	}
+
+	public void setMailServerProperties() {
+
+		String emailPort = "587";//gmail's smtp port
+
+		emailProperties = System.getProperties();
+		emailProperties.put("mail.smtp.port", emailPort);
+		emailProperties.put("mail.smtp.auth", "true");
+		emailProperties.put("mail.smtp.starttls.enable", "true");
+
+	}
+
+	public void createEmailMessage(String[] toEmails) throws AddressException,
+			MessagingException {
+		//String[] toEmails = { "ginji_volts@hotmail.com","boonhui.koh.2013@sis.smu.edu.sg" };
+		String emailSubject = "Outstanding Payment";
+		String emailBody = "Dear User,</br>"+ " This is an email sent by JavaMail api.</br></br>Regards,</br>Admin</br>"
+				+ "</br>This is a system-generated email; please DO NOT REPLY to this email.";
+
+		mailSession = Session.getDefaultInstance(emailProperties, null);
+		try {
+		emailMessage = new MimeMessage(mailSession);
+
+		for (int i = 0; i < toEmails.length; i++) {
+			emailMessage.addRecipient(Message.RecipientType.BCC, new InternetAddress(toEmails[i]));
+		}
+
+		emailMessage.setSubject(emailSubject);
+		emailMessage.setContent(emailBody, "text/html");//for a html email
+		//emailMessage.setText(emailBody);// for a text email
+		 emailMessage.setFrom(new InternetAddress("no_reply@kohBus_DaoBaoApp.com", "KOH BUS DABAO APP"));
+		 
+         emailMessage.setReplyTo(InternetAddress.parse("no_reply@kohBus_DaoBaoApp.com", false));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public void sendEmail() throws AddressException, MessagingException {
+
+		String emailHost = "smtp.gmail.com";
+		String fromUser = "dabaomealorderapp";//just the id alone without @gmail.com
+		String fromUserEmailPassword = "{gxuvimr}";
+
+		Transport transport = mailSession.getTransport("smtp");
+
+		transport.connect(emailHost, fromUser, fromUserEmailPassword);
+		transport.sendMessage(emailMessage, emailMessage.getAllRecipients());
+		transport.close();
+		System.out.println("Email sent successfully.");
+	}
+
+}
