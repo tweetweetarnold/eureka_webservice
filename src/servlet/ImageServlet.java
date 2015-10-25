cf1adcfd88c9a8acb860b7d7cb029f6875f71e45
bhkoh <boonhui.koh.2013@sis.smu.edu.sg>
2015-10-25 18:39:31 +0800

    added the ImageServlet to process the retrieving and loading of images from database

---
+++ src/servlet/ImageServlet.java
@@ -0,0 +1,53 @@
+package servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import controller.FoodController;
+import model.Food;
+
+/**
+ * Servlet implementation class ImageServlet
+ */
+@WebServlet(name = "ImageServlet/*", urlPatterns = { "/ImageServlet/*" })
+public class ImageServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ImageServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		String input = (String) request.getParameter("id");
+		Integer id = Integer.parseInt(input);
+		FoodController foodController = new FoodController();
+		byte[] imageBytes = foodController.getFoodImage(id);
+		response.setContentType("image/jpeg");
+		response.setContentLength(imageBytes.length);
+	
+
+		response.getOutputStream().write(imageBytes);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
