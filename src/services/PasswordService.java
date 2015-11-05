c9affa287a76337fc5c926c3eac727fc47263d58
bunnykillher <bunnykillher@gmail.com>
2015-11-05 13:58:20 +0800

    Modified Password services

--- src/services/PasswordService.java
+++ src/services/PasswordService.java
@@ -16,30 +16,30 @@ public class PasswordService {
 	 * returns a ciphertext in the form of a String object
 	 */
 	public static String encryptPassword(String inputPassword) {
-
-		try {
-			// String text = "Hello World";
-			String key = "Bar12345Bar12345"; // 128 bit key
-
-			// Create key and cipher
-			Key aesKey = new SecretKeySpec(key.getBytes(), "AES");
-			Cipher cipher = Cipher.getInstance("AES");
-
-			// encrypt the text
-			cipher.init(Cipher.ENCRYPT_MODE, aesKey);
-			byte[] encrypted = cipher.doFinal(inputPassword.getBytes());
-			// System.err.println(new String(encrypted));
-			return new String(encrypted);
-
-			// decrypt the text
-			// cipher.init(Cipher.DECRYPT_MODE, aesKey);
-			// String decrypted = new String(cipher.doFinal(encrypted));
-			// System.err.println(decrypted);
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return null;
+		return inputPassword;
+//		try {
+//			// String text = "Hello World";
+//			String key = "Bar12345Bar12345"; // 128 bit key
+//
+//			// Create key and cipher
+//			Key aesKey = new SecretKeySpec(key.getBytes(), "AES");
+//			Cipher cipher = Cipher.getInstance("AES");
+//
+//			// encrypt the text
+//			cipher.init(Cipher.ENCRYPT_MODE, aesKey);
+//			byte[] encrypted = cipher.doFinal(inputPassword.getBytes());
+//			// System.err.println(new String(encrypted));
+//			return new String(encrypted);
+//
+//			// decrypt the text
+//			// cipher.init(Cipher.DECRYPT_MODE, aesKey);
+//			// String decrypted = new String(cipher.doFinal(encrypted));
+//			// System.err.println(decrypted);
+//
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//		return null;
 	}
 
 	/*
@@ -47,25 +47,26 @@ public class PasswordService {
 	 * decrpytion returns a plaintext in the form of String object
 	 */
 	public static String decryptPassword(String encryptedPassword) {
+		return encryptedPassword;
 
-		try {
-			// String text = "Hello World";
-			String key = "Bar12345Bar12345"; // 128 bit key
-
-			// Create key and cipher
-			Key aesKey = new SecretKeySpec(key.getBytes(), "AES");
-			Cipher cipher = Cipher.getInstance("AES");
-
-			// decrypt the text
-			cipher.init(Cipher.DECRYPT_MODE, aesKey);
-			byte[] encrypted = encryptedPassword.getBytes();
-			String decrypted = new String(cipher.doFinal(encrypted));
-			// System.err.println(decrypted);
-			return decrypted;
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return null;
+//		try {
+//			// String text = "Hello World";
+//			String key = "Bar12345Bar12345"; // 128 bit key
+//
+//			// Create key and cipher
+//			Key aesKey = new SecretKeySpec(key.getBytes(), "AES");
+//			Cipher cipher = Cipher.getInstance("AES");
+//
+//			// decrypt the text
+//			cipher.init(Cipher.DECRYPT_MODE, aesKey);
+//			byte[] encrypted = encryptedPassword.getBytes();
+//			String decrypted = new String(cipher.doFinal(encrypted));
+//			// System.err.println(decrypted);
+//			return decrypted;
+//
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//		return null;
 	}
 }
