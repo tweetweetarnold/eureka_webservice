052775c414d740834690870d24a1b952d9d9eca8
tweetweetarnold <arnold.lee.wt@gmail.com>
2015-11-05 11:47:51 +0800

    Included new hibernate config for chris, direct processinserttestdataservlet to login.jsp

--- src/servlet/ProcessInsertTestDataServlet.java
+++ src/servlet/ProcessInsertTestDataServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import test.Test;
 
@@ -17,38 +18,46 @@ import test.Test;
 @WebServlet("/ProcessInsertTestDataServlet")
 public class ProcessInsertTestDataServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ProcessInsertTestDataServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doProcess(request,response);
+	public ProcessInsertTestDataServlet() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doProcess(request,response);
+		doProcess(request, response);
 	}
-	
-	protected void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doProcess(request, response);
+	}
+
+	protected void doProcess(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.setContentType("text/html");
+		HttpSession session = request.getSession();
 		PrintWriter out = response.getWriter();
-		
+
 		Test.main(null);
-		out.println("Database is overridden with data from Test.java.");
+
+		session.setAttribute("success", "Database is overridden with data from Test.java.");
+		response.sendRedirect("login.jsp");
 	}
 
 }
