463288da11311dc28e3d3ce9f05236013366f583
tweetweetarnold <arnold.lee.wt@gmail.com>
2015-12-05 16:54:40 +0800

    introduced catch exception at AESAlgorithm

--- src/services/AESAlgorithm.java
+++ src/services/AESAlgorithm.java
@@ -13,33 +13,44 @@ public class AESAlgorithm {
 	String secret = "agsjdjegqbentkdh";
 	public byte[] keyValue = secret.getBytes();
 
-//	public AESAlgorithm(String key) {
-//		keyValue = key.getBytes();
-//	}
+	// public AESAlgorithm(String key) {
+	// keyValue = key.getBytes();
+	// }
 
 	public Key generateKey() throws Exception {
 		Key key = new SecretKeySpec(keyValue, algo);
 		return key;
 	}
-	
-	public String encrypt(String password) throws Exception{
-		Key key = generateKey();
-		Cipher c = Cipher.getInstance(algo);
-		c.init(Cipher.ENCRYPT_MODE, key);
-		byte[] encVal = c.doFinal(password.getBytes());
-		String encryptedValue = new BASE64Encoder().encode(encVal);		
+
+	public String encrypt(String password) {
+		String encryptedValue = null;
+		try {
+			Key key = generateKey();
+			Cipher c = Cipher.getInstance(algo);
+			c.init(Cipher.ENCRYPT_MODE, key);
+			byte[] encVal = c.doFinal(password.getBytes());
+			encryptedValue = new BASE64Encoder().encode(encVal);
+		} catch (Exception e) {
+			System.out.println("Error occurred at AES Encryption: " + e.getMessage());
+			e.printStackTrace();
+		}
 		return encryptedValue;
 	}
-	
-	public String Decrypt(String encryptedPassword) throws Exception{
-		Key key = generateKey();
-		Cipher c = Cipher.getInstance(algo);
-		c.init(Cipher.DECRYPT_MODE, key);
-		byte[] decodedValue = new BASE64Decoder().decodeBuffer(encryptedPassword);
-		byte[] decVal = c.doFinal(decodedValue);
-		String decryptedPassword = new String(decVal);
+
+	public String decrypt(String encryptedPassword) {
+		String decryptedPassword = null;
+		try {
+			Key key = generateKey();
+			Cipher c = Cipher.getInstance(algo);
+			c.init(Cipher.DECRYPT_MODE, key);
+			byte[] decodedValue = new BASE64Decoder().decodeBuffer(encryptedPassword);
+			byte[] decVal = c.doFinal(decodedValue);
+			decryptedPassword = new String(decVal);
+		} catch (Exception e) {
+			System.out.println("Error occurred at AES Encryption: " + e.getMessage());
+			e.printStackTrace();
+		}
 		return decryptedPassword;
 	}
-	
-	
+
 }
