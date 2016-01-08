ddd81b03eb411dc6b39ff22fb58dff7b6dabb2c2
bunnykillher <bunnykillher@gmail.com>
2016-01-08 10:59:42 +0800

    Updated Quartz service

---
+++ src/services/Quartz.java
@@ -0,0 +1,64 @@
+package services;
+
+import java.util.Date;
+
+import org.quartz.CronScheduleBuilder;
+import org.quartz.JobBuilder;
+import org.quartz.JobDetail;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.quartz.SimpleScheduleBuilder;
+import org.quartz.SimpleTrigger;
+import org.quartz.Trigger;
+import org.quartz.TriggerBuilder;
+import org.quartz.impl.StdSchedulerFactory;
+import org.quartz.impl.triggers.SimpleTriggerImpl;
+
+import background.CloseWindow;
+
+
+public class Quartz {
+	
+	public void addRepeatingJob(String jobIdentity, String triggerIdentity, String cron, Class newClass) throws SchedulerException{
+		// TODO Auto-generated method stub
+		JobDetail job = JobBuilder.newJob(newClass)
+				.withIdentity(jobIdentity, "group1").build();
+		
+		Trigger trigger = TriggerBuilder 
+				.newTrigger()
+				.withIdentity(triggerIdentity, "group1")
+				.withSchedule(
+						CronScheduleBuilder.cronSchedule(cron))
+				.build();
+
+		Scheduler scheduler = new StdSchedulerFactory().getScheduler();
+		scheduler.start();
+		scheduler.scheduleJob(job, trigger);
+		
+	}
+	
+	public void addClosingDate(String jobIdentity, String triggerIdentity, Date date, String id) throws SchedulerException, InterruptedException{
+		// TODO Auto-generated method stub
+		JobDetail job = JobBuilder.newJob(CloseWindow.class)
+				.withIdentity(jobIdentity, "group1").build();
+		Date after = new Date(date.getTime()+10*300);
+		SimpleTrigger trigger = (SimpleTrigger) TriggerBuilder.newTrigger().withIdentity("rahimtrigger", "group1")
+			    .startAt(date).build();
+				
+				
+		System.out.println(date + "Here");
+		Scheduler scheduler = new StdSchedulerFactory().getScheduler();
+		job.getJobDataMap().put(CloseWindow.ID, id);
+		scheduler.scheduleJob(job, trigger);
+		scheduler.start();
+		try {
+			//wait for 30 seconds to finish the job
+			Thread.sleep(1);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	
+		System.out.println("Here");
+	}
+	
+}
--- src/test/QuartzExample.java
+++ src/test/QuartzExample.java
@@ -1,5 +1,8 @@
 package test;
 
+import java.util.Date;
+
+import org.joda.time.DateTime;
 import org.quartz.JobBuilder;
 import org.quartz.JobDetail;
 import org.quartz.Scheduler;
@@ -11,26 +14,46 @@ import org.quartz.impl.StdSchedulerFactory;
 
 import background.HelloJob;
 import background.NewJob;
+import services.Quartz;
 
 public class QuartzExample {
+	
+	public static void main(String[] args){
+		Quartz quartz = new Quartz();
+		try {
+			String windowID = "1";
+			DateTime endDate = new DateTime(2016, 1, 7, 14, 58, 30);
+			Date date = endDate.toDate();
+			quartz.addClosingDate("Hello1", "HelloTrigger", date, windowID);
+			quartz.addRepeatingJob("Hello", "HelloTrigger", "0/5 * * * * ?", HelloJob.class);
+		} catch (SchedulerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+	
+	
+	
 
-	public void runThis() throws SchedulerException{
-		// TODO Auto-generated method stub
-		JobDetail job = JobBuilder.newJob(NewJob.class)
-				.withIdentity("dummyJobName", "group1").build();
-		
-		Trigger trigger = TriggerBuilder 
-				.newTrigger()
-				.withIdentity("dummyTriggerName", "group1")
-				.withSchedule(
-					SimpleScheduleBuilder.simpleSchedule()
-						.withIntervalInSeconds(5).repeatForever())
-				.build();
-
-		Scheduler scheduler = new StdSchedulerFactory().getScheduler();
-		scheduler.start();
-		scheduler.scheduleJob(job, trigger);
-		
-	}
-
+//	public void runThis() throws SchedulerException{
+//		// TODO Auto-generated method stub
+//		JobDetail job = JobBuilder.newJob(NewJob.class)
+//				.withIdentity("dummyJobName", "group1").build();
+//		
+//		Trigger trigger = TriggerBuilder 
+//				.newTrigger()
+//				.withIdentity("dummyTriggerName", "group1")
+//				.withSchedule(
+//					SimpleScheduleBuilder.simpleSchedule()
+//						.withIntervalInSeconds(5).repeatForever())
+//				.build();
+//
+//		Scheduler scheduler = new StdSchedulerFactory().getScheduler();
+//		scheduler.start();
+//		scheduler.scheduleJob(job, trigger);
+//		
+//	}
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		}
 }
