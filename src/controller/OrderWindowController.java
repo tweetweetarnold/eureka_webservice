deda9dff1351cf25cbab844b511c10e71b5e0f10
bunnykillher <bunnykillher@gmail.com>
2016-02-05 12:10:59 +0800

    edit order window servlet and controller

--- src/controller/OrderWindowController.java
+++ src/controller/OrderWindowController.java
@@ -8,6 +8,7 @@ import model.OrderWindow;
 import model.PriceModifier;
 
 import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
 
 import value.StringValues;
 import dao.OrderWindowDAO;
@@ -27,8 +28,8 @@ public class OrderWindowController {
 	public OrderWindowController() {
 	}
 
-	public boolean checkForOrderWindowAvailability(DateTime startTime, DateTime endTime,
-			Company coy, int weeks) throws Exception {
+	public boolean checkForOrderWindowAvailability(DateTime startTime, DateTime endTime, Company coy, int weeks)
+			throws Exception {
 		ArrayList<OrderWindow> allOrderWindows = orderWindowDAO.getAllOrderWindowsUnderCompany(coy);
 		// ArrayList<OrderWindow> occupiedSlots = new ArrayList<OrderWindow>();
 		System.out.println("startDate: " + startTime);
@@ -126,8 +127,8 @@ public class OrderWindowController {
 		return true;
 	}
 
-	public ArrayList<OrderWindow> checkOrderWindowAvailability(DateTime startTime,
-			DateTime endTime, Company coy) throws Exception {
+	public ArrayList<OrderWindow> checkOrderWindowAvailability(DateTime startTime, DateTime endTime, Company coy)
+			throws Exception {
 		ArrayList<OrderWindow> allOrderWindows = orderWindowDAO.getAllOrderWindowsUnderCompany(coy);
 		ArrayList<OrderWindow> occupiedSlots = new ArrayList<OrderWindow>();
 		System.out.println("startDate: " + startTime);
@@ -169,9 +170,8 @@ public class OrderWindowController {
 		return occupiedSlots;
 	}
 
-	public void createNewOrderWindow(DateTime startDate, DateTime endDate, Company company,
-			Canteen canteen, int numberOfWeeks, String remarks, double discountPercentage,
-			double discountAbsolute) {
+	public void createNewOrderWindow(DateTime startDate, DateTime endDate, Company company, Canteen canteen,
+			int numberOfWeeks, String remarks, double discountPercentage, double discountAbsolute) {
 		if (numberOfWeeks > 0) {
 			ArrayList<DateTime> startTimeList = new ArrayList<DateTime>();
 			ArrayList<DateTime> endTimeList = new ArrayList<DateTime>();
@@ -187,27 +187,26 @@ public class OrderWindowController {
 			}
 
 			for (int i = 0; i < startTimeList.size(); i++) {
-				orderWindowDAO.saveOrderWindow(new OrderWindow(startTimeList.get(i), endTimeList
-						.get(i), company, canteen, discountPercentage, discountAbsolute, remarks,
-						null));
+				orderWindowDAO.saveOrderWindow(new OrderWindow(startTimeList.get(i), endTimeList.get(i), company,
+						canteen, discountPercentage, discountAbsolute, remarks, null));
 			}
 
 		} else {
-			orderWindowDAO.saveOrderWindow(new OrderWindow(startDate, endDate, company, canteen,
-					discountPercentage, discountAbsolute, remarks, null));
+			orderWindowDAO.saveOrderWindow(new OrderWindow(startDate, endDate, company, canteen, discountPercentage,
+					discountAbsolute, remarks, null));
 		}
 	}
 
-	public void createNewOrderWindow2(DateTime startDate, DateTime endDate, Company company,
-			Canteen canteen, int numberOfWeeks, String remarks, double discountAbsolute) {
+	public void createNewOrderWindow2(DateTime startDate, DateTime endDate, Company company, Canteen canteen,
+			int numberOfWeeks, String remarks, double discountAbsolute) {
 
 		for (int i = 0; i < numberOfWeeks; i++) {
-			OrderWindow window = new OrderWindow(startDate.plusWeeks(i), endDate.plusWeeks(i),
-					company, canteen, remarks, null);
+			OrderWindow window = new OrderWindow(startDate.plusWeeks(i), endDate.plusWeeks(i), company, canteen,
+					remarks, null);
 
 			ArrayList<PriceModifier> list = new ArrayList<PriceModifier>();
-			PriceModifier discountAbsoluteModifier = new PriceModifier("Discount",
-					StringValues.PRICEMODIFIER_ABSOLUTE, discountAbsolute * -1, window);
+			PriceModifier discountAbsoluteModifier = new PriceModifier("Discount", StringValues.PRICEMODIFIER_ABSOLUTE,
+					discountAbsolute * -1, window);
 			list.add(discountAbsoluteModifier);
 			window.setPriceModifierList(list);
 			orderWindowDAO.saveOrderWindow(window);
@@ -231,8 +230,10 @@ public class OrderWindowController {
 	/**
 	 * Get all OrderWindows with status "Opened" for the given Company
 	 * 
-	 * @param company The company whose "Opened" OrderWindows are to be retrieved
-	 * @return Returns an ArrayList of OrderWindows with status "Opened" for the given Company
+	 * @param company
+	 *            The company whose "Opened" OrderWindows are to be retrieved
+	 * @return Returns an ArrayList of OrderWindows with status "Opened" for the
+	 *         given Company
 	 */
 	public ArrayList<OrderWindow> getAllOpenedWindowsForCompany(Company company) {
 		return orderWindowDAO.getAllOpenedWindowsForCompany(company);
@@ -250,11 +251,86 @@ public class OrderWindowController {
 	/**
 	 * Retrieves the OrderWindow based on the provided ID
 	 * 
-	 * @param orderWindowId The ID of the OrderWindow
+	 * @param orderWindowId
+	 *            The ID of the OrderWindow
 	 * @return The OrderWindow object that has the provided ID
 	 */
 	public OrderWindow getOrderWindow(Integer orderWindowId) {
 		return orderWindowDAO.getOrderWindow(orderWindowId);
+	}
+
+	public void deleteOrderWindow(int orderWindowId) {
+		OrderWindowDAO orderWindowDAO = new OrderWindowDAO();
+		OrderWindow orderWindowToArchive = getOrderWindow(orderWindowId);
+		orderWindowToArchive.setStatus(StringValues.ARCHIVED);
+		orderWindowDAO.updateOrderWindow(orderWindowToArchive);
+	}
+
+	public void editOrderWindow(int orderWindowId, DateTime newStartDate, DateTime newEndDate, Canteen canteen,
+			String remarks, double discountPercentage, double discountAbsolute) throws Exception {
+		OrderWindowDAO orderWindowDAO = new OrderWindowDAO();
+		OrderWindow orderWindowToEdit = getOrderWindow(orderWindowId);
+		DateTime dateEnd = orderWindowToEdit.getEndDate();
+		DateTime dateStart = orderWindowToEdit.getStartDate();
+		DateTimeZone dateTimeZone = DateTimeZone.forID("Asia/Singapore");
+		DateTime currentTime = DateTime.now(dateTimeZone);
+		
+		//to check for clashing orderwindows 
+		ArrayList<OrderWindow> orderWindowList = checkOrderWindowAvailability(newStartDate, newEndDate,
+				orderWindowToEdit.getCompany());
+		
+		if (dateStart.isAfter(currentTime)) {
+			// edit OrderWindow orderWindowToEdit
+			if (newStartDate.isBefore(currentTime)) {
+				throw new Exception("Cannot set start date to before current time");
+				// check if new order window clash with other order windows 
+			} else {
+				
+				//if list is empty no clash
+				if (orderWindowList.isEmpty()) {
+					orderWindowToEdit.setCanteen(canteen);
+					orderWindowToEdit.setStartDate(newStartDate);
+					orderWindowToEdit.setEndDate(newEndDate);
+					orderWindowToEdit.setRemarks(remarks);
+					orderWindowToEdit.setDiscount(discountPercentage);
+					orderWindowToEdit.setDiscountAbsolute(discountAbsolute);
+					orderWindowDAO.updateOrderWindow(orderWindowToEdit);
+				//if list has one order window and it is the same order window go ahead and edit it
+				}else if ((orderWindowList.size() == 1) && (orderWindowList.get(0).getWindowId() == orderWindowId)) {
+					orderWindowToEdit.setCanteen(canteen);
+					orderWindowToEdit.setStartDate(newStartDate);
+					orderWindowToEdit.setEndDate(newEndDate);
+					orderWindowToEdit.setRemarks(remarks);
+					orderWindowToEdit.setDiscount(discountPercentage);
+					orderWindowToEdit.setDiscountAbsolute(discountAbsolute);
+					orderWindowDAO.updateOrderWindow(orderWindowToEdit);
+				//if both conditions not satisfied then it clashes with one or more windows  
+				}else{
+					throw new Exception("New order window dates clash with another order window");
+				}
+			}
+		} else if (dateStart.isBefore(currentTime) && dateEnd.isAfter(currentTime)) {
+			// can only edit endDate
+			//if list is empty no clash
+			if (orderWindowList.isEmpty()) {
+				orderWindowToEdit.setEndDate(newEndDate);
+				orderWindowDAO.updateOrderWindow(orderWindowToEdit);
+				
+			//if list has one order window and it is the same order window go ahead and edit it
+			}else if ((orderWindowList.size() == 1) && (orderWindowList.get(0).getWindowId() == orderWindowId)) {
+				orderWindowToEdit.setEndDate(newEndDate);
+				orderWindowDAO.updateOrderWindow(orderWindowToEdit);
+				
+			}
+			//if both conditions not satisfied then it clashes with one or more windows  
+			else{
+				throw new Exception("New order window dates clash with another order window");
+			}
+			
+		} else {
+			throw new Exception("Order Window Cannot be editted");
+		}
+
 	}
 
 }
---
+++ src/servlet/process/admin/ProcessAdminEditOrderWindowServlet.java
@@ -0,0 +1,94 @@
+package servlet.process.admin;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+import controller.CanteenController;
+import controller.CompanyController;
+import controller.OrderWindowController;
+import model.Canteen;
+import model.Company;
+
+/**
+ * Servlet implementation class ProcessAdminEditOrderWindowServlet
+ */
+@WebServlet("/ProcessAdminEditOrderWindowServlet")
+public class ProcessAdminEditOrderWindowServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public ProcessAdminEditOrderWindowServlet() {
+		super();
+
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		CompanyController companyController = new CompanyController();
+		CanteenController canteenController = new CanteenController();
+		OrderWindowController orderWindowController = new OrderWindowController();
+		try {
+			String discountAbsoluteString = request.getParameter("discountAbsolute");
+			String companyId = request.getParameter("company");
+			String canteenId = request.getParameter("canteen");
+			String startDatetimeString = request.getParameter("startDatetime");
+			String endDatetimeString = request.getParameter("endDatetime");
+			String remarks = request.getParameter("remarks");
+			String orderWindowID = request.getParameter("orderWindow");
+
+			// **Important: to format the time to SG timezone
+			DateTimeFormatter formatter = DateTimeFormat.forPattern("dd-MMMM-yyyy HH:mm")
+					.withZone(DateTimeZone.forID("Asia/Singapore"));
+			DateTime startDatetime = formatter.parseDateTime(startDatetimeString);
+			DateTime endDatetime = formatter.parseDateTime(endDatetimeString);
+			
+			Company company = companyController.getCompany(Integer.parseInt(companyId));
+			Canteen canteen = canteenController.getCanteen(Integer.parseInt(canteenId));
+			double discountAbsolute = 0;
+			try {
+				discountAbsolute = Double.parseDouble(discountAbsoluteString);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			
+			
+			
+			orderWindowController.editOrderWindow(Integer.parseInt(orderWindowID), startDatetime, endDatetime, canteen, remarks, 0.0, discountAbsolute);
+			response.sendRedirect("");
+			
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			session.setAttribute("error", e.getMessage());
+			response.sendRedirect("");
+		}
+
+	}
+
+}
