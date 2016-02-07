e06dda50fe048f54f4ac4998043b3796a1c6d7ca
bhkoh <boonhui.koh.2013@sis.smu.edu.sg>
2016-02-07 23:50:02 +0800

    Finetuned the codes in ProcessAdminAddNewStallServlet and ProcessAdminEditStallServlet

--- src/controller/StallController.java
+++ src/controller/StallController.java
@@ -2,7 +2,15 @@ package controller;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
 import model.Canteen;
 import model.Food;
@@ -19,6 +27,7 @@ import dao.StallDAO;
 public class StallController {
 	CloudinaryUpload cloudinaryUpload = new CloudinaryUpload();
 	StallDAO stallDAO = new StallDAO();
+
 	/**
 	 * Creates a default constructor for StallController
 	 */
@@ -28,7 +37,7 @@ public class StallController {
 	public boolean checkStallExists(String inputStallName, Canteen c) {
 		ArrayList<Stall> stallList = getAllActiveStallsUnderCanteen(c);
 		if (stallList != null) {
-			for (Stall s: stallList) {
+			for (Stall s : stallList) {
 				if (inputStallName.equals(s.getName())) {
 					return true;
 				}
@@ -44,78 +53,144 @@ public class StallController {
 	public void deleteStall(Stall s) {
 		stallDAO.deleteStall(s);
 	}
-	
+
 	public ArrayList<Stall> getAllActiveStallsUnderCanteen(Canteen c) {
 		return stallDAO.getAllActiveStallsUnderCanteen(c);
 	}
-	
+
 	/**
 	 * Retrieves all the Stalls from the Canteen
 	 * 
-	 * @param canteen The designated Canteen to retrieve the Stalls
+	 * @param canteen
+	 *            The designated Canteen to retrieve the Stalls
 	 * @return An ArrayList of Stall objects in the designated Canteen
 	 */
 	public ArrayList<Stall> getAllStallsUnderCanteen(Canteen canteen) {
 		return stallDAO.getAllStallsUnderCanteen(canteen);
 	}
-	
-	
+
 	/**
 	 * Retrieves the Stall based on the provided ID
 	 * 
-	 * @param stallId The ID used for retrieving the Stall
+	 * @param stallId
+	 *            The ID used for retrieving the Stall
 	 * @return The Stall object that has the provided ID
 	 */
 	public Stall getStall(int stallId) {
 		return stallDAO.getStall(stallId);
 	}
-	
+
 	public boolean haveChangesInParameters(Stall oldStall, String inputName, long inputContactNo) {
 		boolean hasChanges = false;
 		String currentName = oldStall.getName();
 		long currentContact = oldStall.getContactNo();
-		
+
 		if (!currentName.equals(inputName)) {
 			hasChanges = true;
 		}
-		
+
 		if (currentContact != inputContactNo) {
 			hasChanges = true;
 		}
-		
+
 		return hasChanges;
 	}
-	
+
 	public String[] imageUpload(byte[] image) throws IOException {
 		return cloudinaryUpload.imageUpload(image);
 	}
-	
+
 	public String[] replaceOldImage(String oldPublicId, byte[] image) throws IOException {
 		return cloudinaryUpload.replaceImage(oldPublicId, image);
 	}
-	
+
 	public void saveStall(Stall s) {
 		stallDAO.saveStall(s);
 	}
-	
+
 	public void updateFoodListToStall(Set<Food> foodList, Stall newStall) {
 		stallDAO.setFoodListToStall(foodList, newStall);
 	}
-	
+
 	/**
 	 * Updates the designated Stall object in the Database
 	 * 
-	 * @param s The Stall object to be updated
+	 * @param s
+	 *            The Stall object to be updated
 	 */
 	public void updateStall(Stall s) {
 		stallDAO.updateStall(s);
 	}
-	
+
 	public long validatesContactNumber(String contactNo) throws Exception {
 		if (contactNo.matches("[689][0-9][0-9][0-9][0-9][0-9][0-9][0-9]")) {
 			return Long.parseLong(contactNo);
 		} else {
 			throw new Exception("Invalid contact number");
+		}
+	}
+
+	public void processAddingStall(byte[] image, String[] parameters, int canteenId) throws Exception {
+		CanteenController canteenController = new CanteenController();
+		String[] cloudinaryOutput = new String[2];
+		/*
+		 * cloudinaryOutput[0] stores the image url 
+		 * cloudinaryOutput[1] stores the image publicId
+		 */
+		
+		long contactNo = validatesContactNumber(parameters[2]);
+
+		Canteen c = canteenController.getCanteen(canteenId);
+		boolean stallExists = checkStallExists(parameters[1], c);
+		if (stallExists) {
+			throw new Exception(parameters[1] + " already exists in " + c.getName());
+		}
+		cloudinaryOutput = imageUpload(image);
+
+		Stall s = new Stall(parameters[1], contactNo, c, new HashSet<Food>(), cloudinaryOutput[0], cloudinaryOutput[1]);
+
+		System.out.println("stallname: " + s.getName());
+		System.out.println("saving food...");
+		saveStall(s);
+
+	}
+
+	public void processEditingStall(byte[] image, String[] parameters, Stall stall) throws Exception {
+		String[] cloudinaryOutput = new String[2];
+		/*
+		 * cloudinaryOutput[0] stores the image url 
+		 * cloudinaryOutput[1] stores the image publicId
+		 */
+		long contactNo = validatesContactNumber(parameters[2]);
+
+		boolean hasChanges = haveChangesInParameters(stall, parameters[1], contactNo);
+		if (hasChanges) {
+			deleteStall(stall);
+			if (image.length == 0) {
+				Stall newStall = new Stall(parameters[1], contactNo, stall.getCanteen(), new HashSet<Food>(),
+						stall.getImageDirectory(), stall.getPublicId());
+				if (!stall.getFoodList().isEmpty()) {
+					updateFoodListToStall(stall.getFoodList(), newStall);
+				}
+				saveStall(newStall);
+			} else {
+				cloudinaryOutput = replaceOldImage(stall.getPublicId(), image);
+				Stall newStall = new Stall(parameters[1], contactNo, stall.getCanteen(), new HashSet<Food>(),
+						cloudinaryOutput[0], cloudinaryOutput[1]);
+				if (!stall.getFoodList().isEmpty()) {
+					updateFoodListToStall(stall.getFoodList(), newStall);
+				}
+				saveStall(newStall);
+			}
+		} else {
+			if (image.length == 0) {
+				throw new Exception("The details entered are the same as the current stall details");
+			} else {
+				cloudinaryOutput = replaceOldImage(stall.getPublicId(), image);
+				stall.setImageDirectory(cloudinaryOutput[0]);
+				stall.setPublicId(cloudinaryOutput[1]);
+				updateStall(stall);
+			}
 		}
 	}
 }
